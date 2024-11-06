cd6e3387305d6bfbb38f8139ed0ccfbb64d8c0f9
André Gomes <andrergomes@gmail.com>
2024-11-06 16:29:13 -0300

    First version about LinkedList data structure

---
+++ data-structure-with-java/src/main/java/br/com/andrergomes/linkedList/LinkedList.java
@@ -0,0 +1,24 @@
+package br.com.andrergomes.linkedList;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
+    }
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+}
